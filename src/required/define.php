fc36c6155408b6a8b6be2fba12a75c25e2675d2e
AsierSaizar <asier.saizar.diaz@gmail.com>
2024-07-10 11:25:24 +0200

    masterrea pasatzeko

--- src/required/define.php
+++ src/required/define.php
@@ -1,11 +1,11 @@
 <?php
 
 //Asier
-//define('APP_DIR', $_SERVER['DOCUMENT_ROOT'] . '/Desktop/repertorioMagia/'); //Aplikazioaren karpeta edozein lekutatik atzitzeko.
-//define('HREF_VIEWS_DIR', '/Desktop/repertorioMagia/src/views'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
-//define('HREF_SRC_DIR', '/Desktop/repertorioMagia/src'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
+define('APP_DIR', $_SERVER['DOCUMENT_ROOT'] . '/Desktop/repertorioMagia/'); //Aplikazioaren karpeta edozein lekutatik atzitzeko.
+define('HREF_VIEWS_DIR', '/Desktop/repertorioMagia/src/views'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
+define('HREF_SRC_DIR', '/Desktop/repertorioMagia/src'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
 
 //Ubuntu raspberry PI
-define('APP_DIR', $_SERVER['DOCUMENT_ROOT'] . '/repoMagiaAsier'); //Aplikazioaren karpeta edozein lekutatik atzitzeko.
-define('HREF_VIEWS_DIR', '/repoMagiaAsier/src/views'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
-define('HREF_SRC_DIR', '/repoMagiaAsier/src'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
+//define('APP_DIR', $_SERVER['DOCUMENT_ROOT'] . '/repoMagiaAsier'); //Aplikazioaren karpeta edozein lekutatik atzitzeko.
+//define('HREF_VIEWS_DIR', '/repoMagiaAsier/src/views'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
+//define('HREF_SRC_DIR', '/repoMagiaAsier/src'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
